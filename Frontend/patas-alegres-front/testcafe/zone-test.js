6732ed83c10c029a0c304e6709354e70211bf50e
Tomas Peresin <tomasperesin@gmail.com>
2024-10-13 22:58:21 -0300

    small fix

--- Frontend/patas-alegres-front/testcafe/zone-test.js
+++ Frontend/patas-alegres-front/testcafe/zone-test.js
@@ -19,27 +19,22 @@ test("Verificar que el botón Zona muestra la lista correcta de zonas", async (t
 test('Eliminar la zona "Sur"', async (t) => {
   const zonaSur = Selector("h2").withText("Sur");
 
-  // Una vez seleccionado el contenedor de "Sur", navegamos al botón de eliminar
   const botonEliminar = zonaSur
     .parent("a")
     .parent("div")
     .sibling("div")
     .find("button.btn.btn-danger");
 
-  // Clic en el botón de eliminar
   await t
-    .hover(zonaSur) // Si es necesario para asegurar que el botón es visible
+    .hover(zonaSur)
     .click(botonEliminar);
 
   await t.eval(() => location.reload(true));
 
-  // Espera redirección o cualquier cambio que ocurra tras eliminar
   await t.expect(Selector("h2").withText("Sur").exists).notOk();
 
-  // Si te redirige a la home o a otra página, navega de vuelta a la lista de zonas
   await t.navigateTo("/zone");
 
-  // Validar que la zona "Sur" ya no está presente
   await t.expect(Selector("h2").withText("Sur").exists).notOk();
 });
 
