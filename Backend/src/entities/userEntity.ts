0093f997e0f27a367a6ab0bcaef525b39cdc63c9
Nicolas Herrera <herreranico2703@gmail.com>
2025-04-24 23:14:02 -0300

    CRUD con MySQL - User

--- Backend/src/entities/userEntity.ts
+++ Backend/src/entities/userEntity.ts
@@ -1,11 +1,11 @@
 export class User{
   constructor(
-    public id: number,
-    public username: string,
-    public password: string, 
+    public id_user: number,
+    public username: string, 
     public name: string,
     public surname: string, 
     public email: string,
-    public birthdate: string
+    public birthdate: string,
+    public password: string,
   ){}
 }
---
+++ Backend/src/routes/userRoutes.ts
@@ -0,0 +1,11 @@
+import { Router } from "express"
+import { user_Controller } from "../controllers/userController.js"
+
+export const userRouter = Router()
+
+userRouter.get('/', user_Controller.findAll)
+userRouter.get('/:id', user_Controller.findOne)
+userRouter.post('/', user_Controller.sanitizeUserInput, user_Controller.create)
+userRouter.put('/:id', user_Controller.sanitizeUserInput, user_Controller.update)
+userRouter.patch('/:id', user_Controller.sanitizeUserInput, user_Controller.update)
+userRouter.delete(':id', user_Controller.remove)
