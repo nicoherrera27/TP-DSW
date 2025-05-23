91e24369c65c224f24ca3b05bab259c0283ba230
Nicolas Herrera <herreranico2703@gmail.com>
2025-05-23 16:47:04 -0300

    arreglo bbdd

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -4,7 +4,7 @@ import { userRouter } from './routes/userRoutes.js'
 const app = express()
 app.use(express.json())
 
-app.use('./api/users', userRouter)
+app.use('/api/users', userRouter)
 
 //aca arranca hall
 
@@ -32,10 +32,10 @@ app.get('/api/halls/:id',(req, res) => {
 
 //aca termina
 
-app.use((_, res) => {
-  res.status(404).send({ message: 'Resource not found' })
-  return
-})
+//app.use((_, res) => {
+//  res.status(404).send({ message: 'Resource not found' })
+//  return
+//})
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
