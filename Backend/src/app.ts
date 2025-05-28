fc2346abb9a24138ba657ac51b944b8db0a6d769
Candela-Boscarol <boscarolcande@gmail.com>
2025-05-28 17:29:42 -0300

    crudgender

--- Backend/src/app.ts
+++ Backend/src/app.ts
@@ -2,6 +2,7 @@ import express, { Request, Response, NextFunction } from 'express'
 import { userRouter } from './user/userRoutes.js'
 import { Hall } from './sala_o_hall/hallEntity.js'
 import { HallRepository } from './sala_o_hall/hallRepository2.js'
+import { Gender } from './gender/genderEntity.js'
 
 const app = express()
 app.use(express.json())
@@ -106,4 +107,29 @@ app.use((_, res) => {
 
 app.listen(3000, () => {
   console.log('Server is running on port 3000')
+})
+
+//aca arranca gender
+
+const genders =  [
+  new Gender (
+  0,
+  'Sala 1'
+  )
+]
+
+app.get('/api/gender', (req,res) => {
+ res.json(genders)
+})
+
+app.get('/api/gender/:id_gender', (req,res) => {
+ const gender = genders.find((gender) => gender.id_gender === parseInt(req.params.id_gender))
+ if(!gender){
+  res.status(404).send({message:'Gender no found'})
+ }
+ res.json(gender)
+})
+
+app.listen (3000, () => {
+  console.log('Server running on http://localhost:3001')
 })
---
+++ Backend/src/gender/genderEntity.ts
@@ -0,0 +1,8 @@
+import crypto from 'node:crypto'
+
+export class Gender {
+    constructor (
+        public id_gender: number,
+        public nom_gender: string,
+    ){}
+}
