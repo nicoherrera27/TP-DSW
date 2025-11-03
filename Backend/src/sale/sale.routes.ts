5082f3c4d5b6d50044e27caa37901425497129a6
Joaco-Araguas <joaquinaraguas18@gmail.com>
2025-11-02 21:07:30 -0300

    venta entrada y validaciones 2.0

--- Backend/src/sale/sale.routes.ts
+++ Backend/src/sale/sale.routes.ts
@@ -1,4 +1,5 @@
 import { Router } from "express";
+import { authMiddleware } from "../shared/middleware/auth.js";
 import { sanitizeSaleInput, findAll, findOne, create, update, remove, createSimulatedSale } from "./sale.controller.js";
 
 export const saleRouter = Router()
@@ -10,5 +11,5 @@ saleRouter.put('/:id',sanitizeSaleInput, update)
 saleRouter.patch('/:id', sanitizeSaleInput, update)
 saleRouter.delete('/:id', remove)
 
-saleRouter.post('/create-sale-simulated', createSimulatedSale);
+saleRouter.post('/create-sale-simulated', authMiddleware,createSimulatedSale);
 
